df40c86b48f947dc5ea107d1a6bf7765bfbdbd75
Farid0558475484 <faridmh@code.edu.az>
2022-08-14 23:17:44 +0400

    sing.song

---
+++ sing.song/Singer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace sing.song
+{
+    public class Singer {
+
+
+        string _name;
+        string _surname;
+        int _age;
+
+        public  Name(string name)
+
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return (_name);
+            }
+
+            set
+            {
+                if (value.Length > 3 && value.Length < 100)
+                {
+                    Name = value;
+                }
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return (_surname);
+
+            }
+            set
+            {
+                if (value.Length > 3 && value.Length < 100)
+                {
+                    Surname = value;
+                }
+
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return (_age);
+
+            }
+            set
+            {
+
+                if (value.Length > 0 && value.Length < 170)
+                {
+                    Age = value;
+                }
+            }
+        }
+
+    }
+
+}
---
+++ sing.song/Song.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace sing.song
+{
+    public class Song
+    {
+        String _name;
+        String _genre;
+        String _singer;
+
+
+        
+        public Name(string name)
+        {
+            Name = name;
+        }
+        public string Name
+        {
+            get
+            {
+
+                return (_name);
+            }
+            set
+            {
+
+                if(value.Length > 0 && value.Length<100)
+                {
+                    Name = value;
+                }
+            }
+        }
+
+        public Genre(string genre)
+        {
+            Genre = genre;
+
+        }
+        public string Genre
+        {
+            get
+            {
+
+                return (_genre);
+
+            }
+            set
+            {
+                if (Pop(value) && Rock(value) && Jazz(value) && Techno(value))
+                {
+                    Pop = value;
+                    Rock = value;
+                    Jazz = value;
+                    Techno = value;
+                        
+
+
+                }
+            }
+        }
+    }
+}
